32e7b808615f4069b65fa1b6be94424bb501ed0b
Jonnie Spratley <jonniespratley@gmail.com>
2022-06-13 10:33:35 -0700

    yarn2

---
+++ packages/jsapps-core/src/Card.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface ICardProps {
+}
+
+export function Card (props: ICardProps) {
+  return (
+    <div>
+      This is a card
+    </div>
+  );
+}
--- packages/jsapps-core/src/index.tsx
+++ packages/jsapps-core/src/index.tsx
@@ -1,3 +1,5 @@
 import * as React from 'react';
+
 export * from './Button';
+export * from './Card';
 export * from './SomeWidget';
